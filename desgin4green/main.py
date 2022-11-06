850b3294342928090c096df1e8efe151a64cb7d1
Etienne Ralec <95361549+tienou29@users.noreply.github.com>
2022-11-06 11:22:25 +0100

    <head> favicon; description; keywords

--- desgin4green/main.py
+++ desgin4green/main.py
@@ -17,7 +17,7 @@ import base64
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 bd_cleanedbonnespratiques = current_dir / "assets" / "bonnespratiquesCLEANED.csv"
 css_file = current_dir / "styles" / "style.css"
-background = current_dir / "assets" / "background.jpg"
+background = current_dir / "assets" / "background1.jpg"
 
 # --- WEBSITE TRASH TEST ---
 
@@ -25,15 +25,43 @@ df = pd.read_csv(bd_cleanedbonnespratiques)
 
 
 def main(background):
+    
+    @st.experimental_memo
+    def get_img_as_base64(file):
+        with open(file, "rb") as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
 
 
-    # --- LOAD CSS, PDF & PROFIL PIC ---
+    img = get_img_as_base64(background)
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+    background-image: url("data:image/png;base64,{img}");
+    background-size: 180%;
+    background-position: top left;
+    background-repeat: repeat;
+    background-attachment: local;
+    }}
+    
+    [data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+    }}
+    [data-testid="stToolbar"] {{
+    right: 2rem;
+    }}
+    </style>
+    """
+    
+
+    # --- LOAD CSS, PDF ---
     with open(css_file) as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
     background = Image.open(background)
-
-    st.markdown("<h1 style='text-align: center; color: green;'>Les bonnes pratiques d'écoconception</h1>", unsafe_allow_html=True)
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: white;'>Les bonnes pratiques d'écoconception</h1>", unsafe_allow_html=True)
     
     
     selector = st.selectbox('Famille', df['Famille'].unique())
@@ -53,14 +81,15 @@ def main(background):
             
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
     
+    st.markdown("<h4 style='color: white;'>Toutes les bonnes pratiques, faîtes votre trie !</h4>", unsafe_allow_html=True)
+    st.write(values)
     
-    st.write('### Full Dataset', values)
     
-    selected_indices = st.multiselect('''Ecrivez l'index du service souhaité:''', df.index)
+    selected_indices = st.multiselect('''Selectionnez l'index de la bonne pratique souhaitée !''',df.index)
     selected_rows = df.loc[selected_indices]
-    st.write('### Votre Panier', selected_rows)
+    st.write("### Votre panier ", selected_rows)
     
-    st.write('#### Télécharger votre panier')
+    st.write('### Télécharger votre panier')
     
     def convert_df(df):
         return df.to_csv(index=False).encode('utf-8')
